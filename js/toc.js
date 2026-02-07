87b5436dd5d64a4297417b6dc6ad0225dc9ce94f
DavidKim <daavkim@icloud.com>
2026-02-08 02:50:32 +0900

    ㅇ

--- js/toc.js
+++ js/toc.js
@@ -1,6 +1,11 @@
+const box = document.querySelector(".category-aside");
 const aside = document.querySelector(".category-aside aside");
+if (!box || !aside) return;
+
+box.classList.remove("hidden");
 
 const titles = document.querySelectorAll("h2, h3, h4");
+if (titles.length === 0) return;
 
 aside.innerHTML = "목차 테스트";
 
