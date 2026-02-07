2c54792a9b3dc556a7d1eb7b3bb104473c435b28
DavidKim <daavkim@icloud.com>
2026-02-08 02:26:19 +0900

    ㅇ

--- js/toc.js
+++ js/toc.js
@@ -1,30 +1,43 @@
-setTimeout(() => {
-  const headings = document.querySelectorAll("h2, h3, h4");
-  if (headings.length === 0) return;
+const target = document.getElementById("contents");
 
-  const toc = document.createElement("div");
-  toc.id = "toc";
-  toc.innerHTML = "<strong>목차</strong>";
+if (!target) {
+  console.log("contents 없음");
+} else {
+  const observer = new MutationObserver(() => {
+    const headings = target.querySelectorAll("h2, h3, h4");
+    if (headings.length === 0) return;
 
-  const ul = document.createElement("ul");
+    observer.disconnect();
 
-  headings.forEach((h, i) => {
-    const id = "toc-" + i;
-    h.id = id;
+    const toc = document.createElement("div");
+    toc.id = "toc";
+    toc.innerHTML = "<strong>목차</strong>";
 
-    const li = document.createElement("li");
-    li.style.marginLeft =
-      h.tagName === "H3" ? "10px" :
-      h.tagName === "H4" ? "20px" : "0px";
+    const ul = document.createElement("ul");
 
-    const a = document.createElement("a");
-    a.href = "#" + id;
-    a.innerText = h.innerText;
+    headings.forEach((h, i) => {
+      const id = "toc-" + i;
+      h.id = id;
 
-    li.appendChild(a);
-    ul.appendChild(li);
+      const li = document.createElement("li");
+      li.style.marginLeft =
+        h.tagName === "H3" ? "10px" :
+        h.tagName === "H4" ? "20px" : "0px";
+
+      const a = document.createElement("a");
+      a.href = "#" + id;
+      a.innerText = h.innerText;
+
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+
+    toc.appendChild(ul);
+    document.body.appendChild(toc);
   });
 
-  toc.appendChild(ul);
-  document.body.appendChild(toc);
-}, 500);
+  observer.observe(target, {
+    childList: true,
+    subtree: true,
+  });
+}
